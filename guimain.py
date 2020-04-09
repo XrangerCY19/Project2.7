37affd92682ac08d5a43e5fa2bc4695e1e6cde16
XrangerCY19 <38623836+XrangerCY19@users.noreply.github.com>
2020-04-10 03:06:53 +0530

    Update guimain.py

--- guimain.py
+++ guimain.py
@@ -10,7 +10,9 @@ from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.core.window import Window
 
+Window.clearcolor = (.1, .1, .1, 1) 
 
 global vsrc  				#Video Source
 vsrc = ObjectProperty(None)
